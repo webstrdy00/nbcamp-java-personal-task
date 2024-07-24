66e2263cf43801cdf395930e02bd6e88b34e0870
webstrdy00 <les_roka22@naver.com>
2024-07-24 19:08:09 +0900

    Level1 - 1

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,16 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int num1 = scanner.nextInt();  // 첫번째 숫자 저장
+
+        System.out.print("두 번쨰 숫자를 입력하세요: ");
+        int num2 = scanner.nextInt();  // 두번쨰 숫자 저장
     }
 }
