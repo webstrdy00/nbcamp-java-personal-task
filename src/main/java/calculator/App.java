089c26e485d61674555c387a8cd344625682ac98
webstrdy00 <les_roka22@naver.com>
2024-07-26 20:50:57 +0900

    Level 2-1, 2-2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,7 +7,7 @@ public class App {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> results = new ArrayList<Integer>();   //  연산결과를 무한히 저장하는 배열
+        Calculator calculator = new Calculator();
 
         while (true){   // while문을 통해 반복 실행
             System.out.print("첫 번째 숫자를 입력 (종료 : 'exit', 첫 번째 결과 삭제 : 'remove', 모든 결과 출력 : 'inquiry') ");
@@ -16,20 +16,21 @@ public class App {
             if (input.equalsIgnoreCase("exit")) {   // input이 exit일 경우 프로그램 종료
                 System.out.println("프로그램이 종료됩니다.");
                 break;
-            } else if (input.equalsIgnoreCase("remove")) { //  input이 remove일 경우 가장 먼저 저장된 결과 삭제
-                if (!results.isEmpty()){   // 배열이 비어있지 않을경우
-                    int removeResult = results.remove(0);   // 가장 먼저 저장된 결과 삭제
-                    System.out.println("가장 먼저 저장된 결과 " + removeResult+ "가 삭제되었습니다.");
-                    System.out.println( );
-                }else{   // 배열이 비어있을 경우
-                    System.out.println("삭제할 결과가 없습니다.");
-                    System.out.println( );
-                }
-                continue;
-            }else if (input.equalsIgnoreCase("inquiry")){ //  input이 inquiry 경우 저장된 연산 결과 전부를 출력
-                printResults(results);
-                continue;
             }
+//             else if (input.equalsIgnoreCase("remove")) { //  input이 remove일 경우 가장 먼저 저장된 결과 삭제
+//                if (!results.isEmpty()){   // 배열이 비어있지 않을경우
+//                    int removeResult = results.remove(0);   // 가장 먼저 저장된 결과 삭제
+//                    System.out.println("가장 먼저 저장된 결과 " + removeResult+ "가 삭제되었습니다.");
+//                    System.out.println( );
+//                }else{   // 배열이 비어있을 경우
+//                    System.out.println("삭제할 결과가 없습니다.");
+//                    System.out.println( );
+//                }
+//                continue;
+//            } else if (input.equalsIgnoreCase("inquiry")){ //  input이 inquiry 경우 저장된 연산 결과 전부를 출력
+//                printResults(results);
+//                continue;
+//            }
 
             int num1;  
             try {       // 첫번째 숫자 저장 과정
@@ -52,45 +53,14 @@ public class App {
             System.out.print("사칙연산 기호를 입력 ('+', '-', '*', '/') ");
             char operator = scanner.next().charAt(0);  // 사칙연산 저장
 
-            int result = 0;
-            boolean validOperation  = true;
-
-            switch (operator){   // switch문을 통해 사칙기호에 따라 연산 진행
-                case '+':
-                    result = num1+num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    if (num2 == 0){
-                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                        validOperation = false;
-                    }else {
-                        result = num1 / num2;
-                    }
-                    break;
-                default:
-                    System.out.println("올바른 사칙연산 기호를 입력하세요.");
-                    validOperation = false;
-                    break;
+            try {       // 계산과정 Calculator클래스 적용
+                int result = calculator.calculate(num1, num2, operator);
+                System.out.printf("%d %c %d = %d\n",num1,operator,num2,result);
+            }catch (CalculatorException e){
+                System.out.println("계산 오류 : "+e.getMessage());
             }
 
-            // 연산 결과를 배열에 저장
-            if(validOperation){
-                results.add(result);
-                System.out.println("결과가 저장되었습니다. (저장된 결과 개수: "+results.size()+")");
-
-            }
             System.out.println( ); // 계산후 빈 줄 추가
-        }
-
-        // 연산 결과 출력
-        for(int i = 0;i< results.size();i++){
-            System.out.println((i+1)+"번째 계산 결과는 "+results.get(i)+"입니다.");
         }
         scanner.close();
     }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,46 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+class CalculatorException extends  Exception{      // 사용자정의 예외 클래스
+    public CalculatorException(String message){
+        super(message);
+    }
+}
 public class Calculator {
+    private List<Integer> results;
 
+    public Calculator(){    // 생성자
+        this.results = new ArrayList<>();
+    }
+
+    public int calculate(int num1, int num2, char operator) throws CalculatorException{     // 계산과정 메서드
+        if (num1 < 0 || num2 < 0)   
+            throw new CalculatorException("음수는 입력할 수 없습니다.");
+
+        int result;
+
+        switch (operator){   // switch문을 통해 사칙기호에 따라 연산 진행
+            case '+':
+                result = num1+num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0){
+                    throw new CalculatorException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                }
+                result = num1 / num2;
+                break;
+            default:
+                throw new CalculatorException("올바른 사칙연산 기호를 입력하세요.");
+        }
+
+        results.add(result);
+        return result;
+    }
 }
