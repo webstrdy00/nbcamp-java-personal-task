d92deafc88df6d5e398683e08b6ee59798c2496a
webstrdy00 <les_roka22@naver.com>
2024-07-27 02:22:47 +0900

    Level 2-7

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -10,54 +10,75 @@ public class App {
         Calculator calculator = new Calculator();
 
         while (true){   // while문을 통해 반복 실행
-            System.out.print("첫 번째 숫자를 입력 (종료 : 'exit', 첫 번째 결과 삭제 : 'remove', 모든 결과 출력 : 'inquiry') ");
-            String input = scanner.next();
+            System.out.print("명령어 입력 ( 사칙연산 : 'calc', 원의 넓이 : 'circle', 결과 출력 : 'inquiry', 결과 삭제 : 'remove', 종료 : 'exit', ) ");
+            String command = scanner.next();
 
-            if (input.equalsIgnoreCase("exit")) {   // input이 exit일 경우 프로그램 종료
+            if (command.equalsIgnoreCase("exit")) {   // command가 exit일 경우 프로그램 종료
                 System.out.println("프로그램이 종료됩니다.");
                 break;
             }
-             else if (input.equalsIgnoreCase("remove")) { //  input이 remove일 경우 가장 먼저 저장된 결과 삭제
-                calculator.removeFirstResult();
-                continue;
-            } else if (input.equalsIgnoreCase("inquiry")){ //  input이 inquiry 경우 저장된 연산 결과 전부를 출력
-                calculator.printResults();
-                continue;
+             else if (command.equalsIgnoreCase("calc")) { //  command가 calc 경우 사칙연산 실행
+                performCalculation(scanner, calculator);
+            } else if (command.equalsIgnoreCase("circle")){ //  command가 circle 경우 원의 넓이 연산 실행
+                performCircleCalculation(scanner, calculator);
+            } else if (command.equalsIgnoreCase("inquiry")) { // command가 inquiry 경우 모든 연산 결과를 출력
+                System.out.print("출력할 결과들을 선택하세요 (사칙연산 : 'calc', 원의 넓이 : 'circle')");
+                String select = scanner.next();
+                if (select.equalsIgnoreCase("calc"))
+                    calculator.printResults();
+                else if (select.equalsIgnoreCase("circle")) {
+                    calculator.printCircleResults();
+                }
+            } else if (command.equalsIgnoreCase("remove")) {
+                System.out.print("삭제할 결과들을 선택하세요 (사칙연산 : 'calc', 원의 넓이 : 'circle')");
+                String select = scanner.next();
+                if (select.equalsIgnoreCase("calc"))
+                    calculator.removeFirstResult();
+                else if (select.equalsIgnoreCase("circle")) {
+                    calculator.removeFirstCircleResults();
+                }
+            } else {
+                System.out.println("올바른 명령어를 입력하세요.");
             }
-
-            int num1;  
-            try {       // 첫번째 숫자 저장 과정
-                num1 = Integer.valueOf(input);
-            }catch (NumberFormatException e){    // 숫자가 아닐경우 오류 처리
-                System.out.println("올바른 숫자를 입력하세요.");
-                continue;
-            }
-
-            System.out.print("두 번쨰 숫자를 입력 : ");
-            input = scanner.next();
-            int num2;  
-            try {       // 두번쨰 숫자 저장 과정
-                num2 = Integer.valueOf(input);
-            }catch (NumberFormatException e){
-                System.out.println("올바른 숫자를 입력하세요.");
-                continue;
-            }
-
-            System.out.print("사칙연산 기호를 입력 ('+', '-', '*', '/') ");
-            char operator = scanner.next().charAt(0);  // 사칙연산 저장
-
-            try {       // 계산과정 Calculator클래스 적용
-                int result = calculator.calculate(num1, num2, operator);
-                System.out.printf("%d %c %d = %d\n",num1,operator,num2,result);
-                System.out.println("결과가 저장되었습니다. (저장된 결과 개수 "+calculator.getResults().size()+")");
-            }catch (CalculatorException e){
-                System.out.println("계산 오류 : "+e.getMessage());
-            }
-
-            System.out.println( ); // 계산후 빈 줄 추가
+            System.out.println( );
         }
         System.out.println("사칙연산 결과: ");
         calculator.printResults();
+
+        System.out.println("원의 넓이 계산 결과: ");
+        calculator.printCircleResults();
         scanner.close();
+    }
+
+    private static void performCalculation(Scanner scanner, Calculator calculator) {    // 사칙연산을 실행하는 메서드
+        System.out.print("첫번째 숫자를 입력하세요.");
+        int num1 = scanner.nextInt();
+        System.out.print("두번째 숫자를 입력하세요");
+        int num2 = scanner.nextInt();
+        System.out.print("사칙연산 기호를 입력 ('+', '-', '*', '/')");
+        char operator = scanner.next().charAt(0);
+
+        try {
+            int result = calculator.calculate(num1, num2, operator);
+            System.out.printf("%d %c %d = %d\n", num1, operator, num2, result);
+            System.out.println("결과가 저장되었습니다. (저장된 결과 개수 "+calculator.getResults().size()+")");
+        }catch (CalculatorException e){
+            System.out.println("계산 오류 : "+e.getMessage());
+        }
+        System.out.println( );
+    }
+
+    private static void performCircleCalculation(Scanner scanner, Calculator calculator) {        // 원의 넓이 계산을 실행하는 메서드
+        System.out.print("반지름을 입력해주세요.");
+        double radius = scanner.nextDouble();
+
+        try {
+            double result = calculator.circleCalculate(radius);
+            System.out.printf("반지름이 %.2f인 원의 넓이: %.2f\n", radius, result);
+            System.out.println("결과가 저장되었습니다. (저장된 결과 개수 "+calculator.getCircleResults().size()+")");
+        }catch (CalculatorException e){
+            System.out.println("계산 오류 : "+e.getMessage());
+        }
+        System.out.println( );
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,10 +8,13 @@ class CalculatorException extends  Exception{      // 사용자정의 예외 클
     }
 }
 public class Calculator {
+    private static final double PI = 3.14159;      // static final를 이용해서 상수로 PI를 정의
     private List<Integer> results;
+    private List<Double> circleResults;
 
     public Calculator(){    // 생성자
         this.results = new ArrayList<>();
+        this.circleResults = new ArrayList<>();
     }
 
     public int calculate(int num1, int num2, char operator) throws CalculatorException{     // 계산과정 메서드
@@ -43,9 +46,21 @@ public class Calculator {
         results.add(result);
         return result;
     }
+
+    public double circleCalculate(double radius) throws CalculatorException{   // 원의 넓이 계산하는 메서드
+        if (radius < 0)
+            throw new CalculatorException("반지름은 음수가 될 수 없습니다.");
+
+        double result = PI * radius * radius;
+        circleResults.add(result);
+
+        return result;
+    }
+
     public List<Integer> getResults(){     // 저장된 모든 결과값을 가져가는 메서드
         return new ArrayList<>(results);
-    }
+    }    // 연산 결과들을 가져오는 메서드
+    public List<Double> getCircleResults(){return new ArrayList<>(circleResults);}   // 원의 넓이 계산 결과들을 가져오는 메서드
 
     public void removeFirstResult(){     // 첫번째로 저장된 결과를 삭제하는 메서드
         if (results.isEmpty()){
@@ -58,11 +73,22 @@ public class Calculator {
         }
     }
 
+    public void removeFirstCircleResults(){       // 원의 넓이 저장된 첫번째 결과를 삭제하는 메서드
+        if (circleResults.isEmpty()){
+            System.out.println("삭제할 값이 없습니다.");
+            System.out.println();
+        }else {
+            circleResults.remove(0);
+            System.out.println("가장 먼저 저장된 결과가 삭제되었습니다.");
+            System.out.println( );
+        }
+    }
+
     public void clearResults(){    // 계산 결과들을 모두 제거하는 메서드
         results.clear();
     }
 
-    public void printResults(){    // 저장된 계산 결과들을 보여주는 메서드
+    public void printResults(){    // 저장된 계산 결과들을 프린트하는 메서드
         if (results.isEmpty()){
             System.out.println("저장된 결과가 없습니다.");
         }else {
@@ -70,6 +96,20 @@ public class Calculator {
             int index = 1;
             for (int result : results){
                 System.out.println(index+"번째 결과는 "+result+"입니다.");
+                index++;
+            }
+            System.out.println( );
+        }
+    }
+    
+    public void printCircleResults(){   // 원의 넓이 계산 결과들을 프린트하는 메서드
+        if (circleResults.isEmpty())
+            System.out.println("저장된 결과가 없습니다.");
+        else {
+            System.out.println("저장된 원의 넓이 결과");
+            int index = 1;
+            for (double result : circleResults){
+                System.out.println(index+"번쨰 결과는 "+result+"입니다.");
                 index++;
             }
             System.out.println( );
